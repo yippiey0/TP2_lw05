dcf41de0fd0c6e4704570187b6223e7ce6a19b0a
kotoro777 <vadim120903@gmail.com>
2023-05-27 22:43:49 +0400

    Добавьте файлы проекта.

---
+++ TP2_lw05/Controllers/DatabaseController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TP2_lw05.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        private static DatabaseModel[] _data = new DatabaseModel[0];
+
+        public IActionResult Index()
+        {
+            return View(_data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(DatabaseModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Array.Resize(ref _data, _data.Length + 1);
+                _data[_data.Length - 1] = model;
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            if (id >= 0 && id < _data.Length)
+            {
+                var model = _data[id];
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (id >= 0 && id < _data.Length)
+            {
+                var model = _data[id];
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, DatabaseModel updatedModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id >= 0 && id < _data.Length)
+                {
+                    _data[id] = updatedModel;
+                    return RedirectToAction("Index");
+                }
+                return NotFound();
+            }
+            return View(updatedModel);
+        }
+    }
+
+
+}
---
+++ TP2_lw05/Models/DatabaseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public class DatabaseModel
+{
+    [DisplayName("Идентификатор")]
+    public int Id { get; set; }
+
+    [DisplayName("Название")]
+    public string Name { get; set; }
+
+    [DisplayName("Дата создания")]
+    [DataType(DataType.Date)]
+    public DateTime CreatedDate { get; set; }
+
+    [DisplayName("Автор")]
+    public string Author { get; set; }
+
+    [DisplayName("Количество записей")]
+    public int RecordCount { get; set; }
+
+    [DisplayName("Рейтинг")]
+    public double Rating { get; set; }
+}
